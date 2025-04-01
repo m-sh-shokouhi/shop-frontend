ba6f34229ddea538614432eaec09a9bf583cdc48
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-04-01 16:49:04 +0330

    add navbar

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
 import "./globals.css";
-
+import { Header } from "@/components"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
+        <Header />
+        <main>
         {children}
+        </main>
       </body>
     </html>
   );
