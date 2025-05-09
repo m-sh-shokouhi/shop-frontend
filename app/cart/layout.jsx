12cb18901e0863eb5cf89364bcb3220125e0a895
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-05-09 16:57:29 +0330

    auto formatting

--- app/cart/layout.jsx
+++ app/cart/layout.jsx
@@ -1,11 +1,10 @@
-import { Stepper } from '@/components/index.js'
-import SiteLayout from '../(shopping)/layout.jsx'
-
-export default function CartLayout({children}) {
+import { Stepper } from "@/components/index.js";
+import SiteLayout from "../(shopping)/layout.jsx";
+export default function CartLayout({ children }) {
   return (
     <SiteLayout>
-        <Stepper/>
-        {children}
+      <Stepper />
+      {children}
     </SiteLayout>
-  )
+  );
 }
