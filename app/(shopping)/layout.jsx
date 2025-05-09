e583cd5909ce00a0e92d81a27e8679b3e98bd205
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-05-09 16:56:11 +0330

    add UserContextProvider to main layout

--- app/(shopping)/layout.jsx
+++ app/(shopping)/layout.jsx
@@ -1,19 +1,22 @@
 import { Container } from "@mui/material";
 import "../globals.css";
-import { Header } from "@/components"
-import { theme } from '@/components/theme/ThemeContext';
-import { ThemeProvider } from '@mui/material/styles';
+import { Header } from "@/components";
+import { theme } from "@/components/theme/ThemeContext";
+import { ThemeProvider } from "@mui/material/styles";
+import { UserContextProvider } from "../_context/UserContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
-        <ThemeProvider theme={theme}>
-          <Header />
-          <Container maxWidth="lg" sx={{marginTop: "10px"}}>
-          {children}
-          </Container>
-        </ThemeProvider>
+        <UserContextProvider>
+          <ThemeProvider theme={theme}>
+            <Header />
+            <Container maxWidth="lg" sx={{ marginTop: "10px" }}>
+              {children}
+            </Container>
+          </ThemeProvider>
+        </UserContextProvider>
       </body>
     </html>
   );
