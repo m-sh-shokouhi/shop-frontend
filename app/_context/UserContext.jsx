7d3e1fd20e2f5c8b7b0d72ae0e63edf5cf27847a
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-05-10 14:38:57 +0330

    add sign in logic to UserContext

--- app/_context/UserContext.jsx
+++ app/_context/UserContext.jsx
@@ -1,37 +1,65 @@
 "use client";
 import { API_URL } from "@/config";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useContext, useState } from "react";
 
 const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(() => localStorage.getItem("token"));
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    if (token && !user) {
-      const fetchUser = async () => {
-        try {
-          const res = await fetch(`${API_URL}auth/me`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          const data = await res.json();
-          if (res.ok) {
-            setUser(data);
-          } else {
-            signOut();
-          }
-        } catch (err) {
-          console.error("Failed to fetch user: ", err);
-          signOut();
-        }
-      };
+    const savedToken = localStorage.getItem("token");
+    if (savedToken) {
+      setToken(savedToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      fetchUser();
     }
   }, [token]);
 
-  const signIn = async (username, password) => {};
+  const fetchUser = async () => {
+    try {
+      const res = await fetch(`${API_URL}auth/me`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setUser(data);
+      } else {
+        signOut();
+      }
+    } catch (err) {
+      console.error("Failed to fetch user: ", err);
+      signOut();
+    }
+  };
+
+  const signIn = async (username, password) => {
+    try {
+      const res = await fetch(`${API_URL}auth/login/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        localStorage.setItem("token", data.token);
+        setToken(data.token);
+        setUser(data.user);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const signUp = async (username, password1, password2) => {};
 
   const signOut = () => {
@@ -39,6 +67,12 @@ export const UserContextProvider = ({ children }) => {
     setUser(null);
     localStorage.removeItem("token");
   };
+
+  return (
+    <UserContext.Provider value={{ user, token, signIn, signUp, signOut }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUser = () => useContext(UserContext);
