df11ec0c30ed1d9a5b3c6d84045be35cc20c8374
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-05-13 09:56:47 +0330

    add CartContext to main layout

--- app/(shopping)/layout.jsx
+++ app/(shopping)/layout.jsx
@@ -4,18 +4,21 @@ import { Header } from "@/components";
 import { theme } from "@/components/theme/ThemeContext";
 import { ThemeProvider } from "@mui/material/styles";
 import { UserContextProvider } from "../_context/UserContext";
+import { CartProvider } from "../_context/CartContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
         <UserContextProvider>
-          <ThemeProvider theme={theme}>
-            <Header />
-            <Container maxWidth="lg" sx={{ marginTop: "10px" }}>
-              {children}
-            </Container>
-          </ThemeProvider>
+          <CartProvider>
+            <ThemeProvider theme={theme}>
+              <Header />
+              <Container maxWidth="lg" sx={{ marginTop: "10px" }}>
+                {children}
+              </Container>
+            </ThemeProvider>
+          </CartProvider>
         </UserContextProvider>
       </body>
     </html>
