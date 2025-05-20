2974e81a1d80ce4add252101b52d60c745f37b92
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-05-20 07:31:57 +0330

    Add Error Provider to layout

--- app/(shopping)/layout.jsx
+++ app/(shopping)/layout.jsx
@@ -5,21 +5,24 @@ import { theme } from "@/components/theme/ThemeContext";
 import { ThemeProvider } from "@mui/material/styles";
 import { UserContextProvider } from "../_context/UserContext";
 import { CartProvider } from "../_context/CartContext";
+import { ErrorProvider } from "../_context/ErrorContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
-        <UserContextProvider>
-          <CartProvider>
-            <ThemeProvider theme={theme}>
-              <Header />
-              <Container maxWidth="lg" sx={{ marginTop: "10px" }}>
-                {children}
-              </Container>
-            </ThemeProvider>
-          </CartProvider>
-        </UserContextProvider>
+        <ErrorProvider>
+          <UserContextProvider>
+            <CartProvider>
+              <ThemeProvider theme={theme}>
+                <Header />
+                <Container maxWidth="lg" sx={{ marginTop: "10px" }}>
+                  {children}
+                </Container>
+              </ThemeProvider>
+            </CartProvider>
+          </UserContextProvider>
+        </ErrorProvider>
       </body>
     </html>
   );
